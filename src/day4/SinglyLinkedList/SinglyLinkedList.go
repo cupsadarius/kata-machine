923e1a4056282c869d8a2b87b36fbba13bc4fdef
Cupsa Darius <cupsadarius@gmail.com>
2025-06-11 15:45:26 +0300

    fix day 4

--- src/day4/SinglyLinkedList/SinglyLinkedList.go
+++ src/day4/SinglyLinkedList/SinglyLinkedList.go
@@ -1,6 +1,8 @@
 package singlylinkedlist
 
 import (
+	"math"
+
 	dsa "github.com/nacknime-official/kata-machine-go/src/DSA"
 )
 
@@ -9,7 +11,7 @@ type Node[T comparable] struct {
 	next  *Node[T]
 }
 
-func newNode[T comparable](item T) *Node[T] {
+func NewNode[T comparable](item T) *Node[T] {
 	return &Node[T]{value: item, next: nil}
 }
 
@@ -23,6 +25,7 @@ func (l *SinglyLinkedList[T]) Len() int { return l.length }
 
 func NewSinglyLinkedList[T comparable]() *SinglyLinkedList[T] {
 	return &SinglyLinkedList[T]{length: 0, head: nil, tail: nil}
+
 }
 
 func (l *SinglyLinkedList[T]) traverseTo(idx int) *Node[T] {
@@ -33,11 +36,12 @@ func (l *SinglyLinkedList[T]) traverseTo(idx int) *Node[T] {
 	for i := 0; i < idx; i++ {
 		current = current.next
 	}
+
 	return current
 }
 
 func (l *SinglyLinkedList[T]) Prepend(item T) {
-	node := newNode(item)
+	node := NewNode(item)
 	l.length++
 	if l.head != nil {
 		node.next = l.head
@@ -45,6 +49,7 @@ func (l *SinglyLinkedList[T]) Prepend(item T) {
 	if l.tail == nil {
 		l.tail = node
 	}
+
 	l.head = node
 }
 
@@ -53,8 +58,8 @@ func (l *SinglyLinkedList[T]) InsertAt(item T, idx int) {
 		l.Prepend(item)
 		return
 	}
+	node := NewNode(item)
 	l.length++
-	node := newNode(item)
 	current := l.traverseTo(idx - 1)
 	node.next = current.next
 	current.next = node
@@ -66,34 +71,38 @@ func (l *SinglyLinkedList[T]) Append(item T) {
 		l.Prepend(item)
 		return
 	}
-	node := newNode(item)
+	node := NewNode(item)
+
 	l.length++
 
 	l.tail.next = node
 	l.tail = node
-
 }
 
 func (l *SinglyLinkedList[T]) Remove(item T) (T, bool) {
 	current := l.head
 	if current.value == item {
-		l.length--
+		l.length = int(math.Max(float64(l.length-1), 0))
 		l.head = current.next
 		current.next = nil
 		return current.value, true
 	}
-	for current.next != nil {
-		if current.next.value == item {
-			l.length--
-			if current.next == l.tail {
-				l.tail = current
-			}
-			removed := current.next
-			current.next = removed.next
-			removed.next = nil
-			return removed.value, true
-		}
+	var newTail *Node[T]
 
+	for current.next != nil {
+		if current.next == l.tail {
+			newTail = current
+		}
+		if current.next.value == item {
+			current.next = current.next.next
+			current.next = nil
+			l.length = int(math.Max(float64(l.length-1), 0))
+			return current.next.value, true
+		}
+		current = current.next
+	}
+	if newTail != nil {
+		l.tail = newTail
 	}
 
 	return dsa.ZeroValue[T](), false
@@ -101,32 +110,31 @@ func (l *SinglyLinkedList[T]) Remove(item T) (T, bool) {
 }
 
 func (l *SinglyLinkedList[T]) Get(idx int) (T, bool) {
-	node := l.traverseTo(idx)
-	if node != nil {
-		return node.value, true
+	current := l.traverseTo(idx)
+	if current != nil {
+		return current.value, true
 	}
 
-	return dsa.ZeroValue[T](), true
+	return dsa.ZeroValue[T](), false
 }
 
 func (l *SinglyLinkedList[T]) RemoveAt(idx int) (T, bool) {
 	if idx == 0 {
+		l.length = int(math.Max(float64(l.length-1), 0))
 		removed := l.head
 		l.head = removed.next
 		removed.next = nil
-		l.length--
 		return removed.value, true
 	}
-	node := l.traverseTo(idx - 1)
-	if node.next != nil {
-		removed := node.next
-		node.next = removed.next
+	current := l.traverseTo(idx - 1)
+	if current.next != nil {
+		l.length = int(math.Max(float64(l.length-1), 0))
+		removed := current.next
+		current.next = removed.next
 		removed.next = nil
-		l.length--
 		return removed.value, true
 	} else {
-
-		l.length--
+		l.length = int(math.Max(float64(l.length-1), 0))
 		removed := l.tail
 		l.tail = l.traverseTo(idx - 2)
 		removed.next = nil
